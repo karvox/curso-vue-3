62dc20bef5c160ea294c754826ebf29bf955a8cb
Judson Borges <judsonborges@ect.ufrn.br>
2022-11-04 18:19:58 -0300

    Api OK

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/users', function(){
+    return User::all();
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
